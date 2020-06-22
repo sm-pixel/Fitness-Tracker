a1fa6a7b02108952172da0c203a9bd4ed9bdb22e
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-22 14:48:32 -0400

    started server file

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongojs = require("mongojs");
+const logger = require("morgan");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+const databaseUrl = "";
+const collections = [""];
+
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
+
+app.get("/", (req, res) => {
+  res.send(index.html);
+});
+
+//Listening on PORT 3000
+app.listen(3000, () => {
+    console.log("App running on port 3000!");
+  });
