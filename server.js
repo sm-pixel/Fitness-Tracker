3479633960073f5bc7cc58067ee51ee6ed0a2fcb
sammi morrison <ms.samantha.marie.86@gmail.com>
2020-06-23 14:42:29 -0400

    installed morgan and updated server js file

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongojs = require("mongojs");
+// const mongojs = require("mongojs");
 const morgan = require("morgan");
 
 const PORT = 3000;
@@ -8,23 +8,23 @@ const app = express();
 
 app.use(morgan("dev"));
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
 app.use(express.static("public"));
 
-const databaseUrl = "";
-const collections = [""];
+// const databaseUrl = "";
+// const collections = [""];
 
-const db = mongojs(databaseUrl, collections);
+// const db = mongojs(databaseUrl, collections);
 
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
+// db.on("error", error => {
+//   console.log("Database Error:", error);
+// });
 
-app.get("/", (req, res) => {
-  res.send(index.html);
-});
+// app.get("/", (req, res) => {
+//   res.send(index.html);
+// });
 
 //Listening on PORT...
 app.listen(PORT, () => {
